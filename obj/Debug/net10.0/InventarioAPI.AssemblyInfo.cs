6a041c5a93c620ca462dfb7bdb8f3bbdfd5ce342
Camilo Castrillon <Camiloc306766@gmail.com>
2026-05-16 13:54:57 -0500

    Feature: Add total value in products Description: Add new endpoint for total value of stock authored: Camilo Castrillon

--- obj/Debug/net10.0/InventarioAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/InventarioAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28859e080498deefca161828bdcb31fa65a291d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
